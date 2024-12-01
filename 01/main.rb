fe33c92cba1d24dc847dbe718516c79d776d0262
jey <code@jnf.dev>
2024-12-01 12:45:08 -0800

    day 01: updates module strucuter to work with the new runner

--- 01/main.rb
+++ 01/main.rb
@@ -8,31 +8,19 @@ def munge(data)
   end
 end
 
-def p1(data)
-  x, y = munge(data)
-  x.sort.zip(y.sort).reduce(0) { |s, (a, b)| s + (a-b).abs }
-end
-
-def p2(data)
-  x, y = munge(data)
-  z = y.reduce(Hash.new(0)) do |a, n|
-    a[n] += 1
-    a
-  end
-  x.reduce(0) { |a, n| a += n * z[n] }
-end
-
 #external API goes down here
 module Solutions
-  def self.fake
-    data = Tools::autofetch
-    p p1(data) # 11
-    p p2(data) # 31
+  def self.p1
+    x, y = munge(Tools::autofetch)
+    p x.sort.zip(y.sort).reduce(0) { |s, (a, b)| s + (a-b).abs }
   end
 
-  def self.real
-    data = Tools::autofetch
-    p p1(data) # 1258579
-    p p2(data) # 23981443
+  def self.p2
+    x, y = munge(Tools::autofetch)
+    z = y.reduce(Hash.new(0)) do |a, n|
+      a[n] += 1
+      a
+    end
+    p x.reduce(0) { |a, n| a += n * z[n] }
   end
 end
